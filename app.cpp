f93810d4653f3c24d861813fcf1fc93bde558cdc
rajesh <prorajeshsarkar@gmail.com>
2024-07-27 09:50:10 +0530

    this is first commit

---
+++ app.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace  std; 
+int main(){
+    int rows;
+    cout<<"Enter youre number:";
+    cin>>rows;
+
+    for( int i = 0; i < rows; i++){
+        for(int j = 0; j <   i; j++){
+            cout <<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ newApp.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main (){
+    int rows,cols;
+
+    cout <<"Enter the Numbers of rows in restaagle- ";
+    cin>> rows;
+
+    cout <<"Enter the number of columns in rectangle -"
+    cin >> cols;
+
+    cout<< "Rectangle of dimenssion" << rows << "*" << cols <<  endl;
+
+    for(int i= 0; i < rows; i++){
+        for (int j =0; j < cols:j++ ){
+            if 
+        }
+    }
+
+}
